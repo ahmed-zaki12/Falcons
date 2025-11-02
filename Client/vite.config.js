049138ba280752ab7ab3a15d607fbb77a2955e54
ahmed-zaki12 <ahmed.zaki98763@gmail.com>
2025-11-02 17:48:44 +0200

    fix: update vite base path for custom domain

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -16,7 +16,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Falcons/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
